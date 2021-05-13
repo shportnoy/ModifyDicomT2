2f42516be41d5e2cb8e2011722b7f7661b191665
shportnoy <shportnoy@gmail.com>
2021-05-13 00:16:46 -0400

    Image Comments field not written for VE11B

--- modify_dicom_t2/modify_dicom_t2.py
+++ modify_dicom_t2/modify_dicom_t2.py
@@ -7,17 +7,50 @@ import pydicom
 import os.path
 import shutil
 import easygui
+import re
+
+def get_prep_times_VE11(dicom_hdr):
+    
+    #define regular expressions
+    prep_times_re = r'sPrepPulses.adT2PrepDuration.__attribute__.size[ ]+=[ ]+([\d]+)'
+    prep_times_value_re = r'sPrepPulses.adT2PrepDuration\[\d+\][ ]+=[ ]+([.\d]+)'
+ 
+    out = repr(dicom_hdr[(0x0029, 0x1020)].value) #This field contains all of MeasYaps
+    out = out.replace('\\t', ' ')
+    out = out.replace('\\n', '\n')
+    
+    prep_time_search = re.findall(prep_times_re, out)
+    if not prep_time_search:
+        return []
+    else:
+        num_preps = int(prep_time_search[0])
+        prep_times = re.findall(prep_times_value_re, out)[0:num_preps]
+        return prep_times
+   
+
 
 def fix_baseline(in_folder, out_folder):
     
     fname_list=os.listdir(in_folder)
-    dicom_list=[fname for fname in fname_list if '.IMA' in fname or '.dcm' in fname] #find all dicoms
+    dicom_list=[fname for fname in fname_list if '.ima' in fname or '.IMA' in fname or '.dcm' in fname] #find all dicoms
     for index, dicom_filename in enumerate(dicom_list): #go through all dicoms, if prep time = 0 (baseline), 
                                                         #change to prep time = 2000
         ds = pydicom.dcmread(os.path.join(in_folder, dicom_filename))
-        comment_str = ds.ImageComments
-        ds.ImageComments = comment_str.replace('0','2000')
-        ds.save_as(os.path.join(out_folder,dicom_filename))
+        
+        if hasattr(ds, 'ImageComments'): #ImageComments field is only pre-written in VE11C
+            
+            comment_str = ds.ImageComments
+            ds.ImageComments = comment_str.replace('0','2000')
+        
+        else:  # VE11B - need to add ImageComments field with prep time
+        
+            if index==0: #get list of all prep times - each dicom file has the list of all prep times 
+                         #in the sequence, so only need to do this once
+                prep_time_list = get_prep_times_VE11(ds)
+                
+            ds.ImageComments = 'T2 prep. duration = ' + prep_time_list[index].split('.')[0] + ' ms'
+            
+        ds.save_as(os.path.join(out_folder, dicom_filename))
 
 
 def main():
